75ec9ce33fd6a96ba20a9658c034dbd82d8d2a2a
JdSeus <julianoseusdev@outlook.com>
2025-08-22 11:23:47 -0300

    Testing Creation of User

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'altrano@email.com', '123456', 'ROLE_ADMIN');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (101, 'beltrano@email.com', '123456', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (102, 'citrano@email.com', '123456', 'ROLE_CLIENTE');

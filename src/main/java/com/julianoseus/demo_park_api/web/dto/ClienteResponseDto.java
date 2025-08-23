8da276c40e2224a653f095650379a6f6df854c73
JdSeus <julianoseusdev@outlook.com>
2025-08-23 10:50:37 -0300

    Work on ClienteDto

---
+++ src/main/java/com/julianoseus/demo_park_api/web/dto/ClienteResponseDto.java
@@ -0,0 +1,20 @@
+package com.julianoseus.demo_park_api.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteResponseDto {
+
+    private Long id;
+    private String name;
+    private String cpf;
+
+
+
+}

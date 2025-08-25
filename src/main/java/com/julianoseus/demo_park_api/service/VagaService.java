371b890a37a142d90e551bcb3c837c715cbfcfa8
JdSeus <julianoseusdev@outlook.com>
2025-08-25 10:20:31 -0300

    Testing Create Vaga

--- src/main/java/com/julianoseus/demo_park_api/service/VagaService.java
+++ src/main/java/com/julianoseus/demo_park_api/service/VagaService.java
@@ -3,9 +3,20 @@ package com.julianoseus.demo_park_api.service;
 import com.julianoseus.demo_park_api.entity.Vaga;
 import com.julianoseus.demo_park_api.exception.EntityNotFoundException;
 import com.julianoseus.demo_park_api.repository.VagaRepository;
+import com.julianoseus.demo_park_api.web.dto.ClienteResponseDto;
+import com.julianoseus.demo_park_api.web.dto.UsuarioResponseDto;
+import com.julianoseus.demo_park_api.web.dto.VagaResponseDto;
 import com.julianoseus.demo_park_api.web.exception.CodigoUniqueViolationException;
+import com.julianoseus.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +26,18 @@ public class VagaService {
 
     private final VagaRepository vagaRepository;
 
+    @Operation(summary = "Criar uma nova vaga", description = "Recurso para criar uma nova vaga. Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Recurso criado com sucesso",
+                        headers = @Header(name = HttpHeaders.LOCATION, description = "URL do recurso criado")
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Vaga já cadastrada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class
+                    ))),
+                    @ApiResponse(responseCode = "422", description = "Recurso não processado por dados de entrada inválidos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class
+                    ))),
+            }
+    )
     @Transactional
     public Vaga salvar(Vaga vaga) {
         try {
@@ -24,6 +47,16 @@ public class VagaService {
         }
     }
 
+    @Operation(summary = "Localizar uma vaga", description = "Recurso para localizar uma vaga pelo seu código. Requisição exige uso de um bearer token. Acesso restrito a Role='ADMIN'",
+            security = @SecurityRequirement(name = "security"),
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Recurso localizado com sucesso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = VagaResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Vaga não localizada", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class
+                    ))),
+                    @ApiResponse(responseCode = "403", description = "Recurso não permitido ao perfil de CLIENTE", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class
+                    ))),
+            }
+    )
     @Transactional(readOnly = true)
     public Vaga buscarPorCodigo(String codigo) {
         return vagaRepository.findByCodigo(codigo).orElseThrow(
---
+++ src/test/resources/sql/vagas/vagas-insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO usuarios (id, username, password, role) VALUES (100, 'altrano@email.com', '$2a$12$3vsHeMzwlnHzMyq/xKkJquiCJBqXzCFRndrihjKhY5Y0F4leJIKg.', 'ROLE_ADMIN');
+INSERT INTO usuarios (id, username, password, role) VALUES (101, 'beltrano@email.com', '$2a$12$3vsHeMzwlnHzMyq/xKkJquiCJBqXzCFRndrihjKhY5Y0F4leJIKg.', 'ROLE_CLIENTE');
+INSERT INTO usuarios (id, username, password, role) VALUES (102, 'citrano@email.com', '$2a$12$3vsHeMzwlnHzMyq/xKkJquiCJBqXzCFRndrihjKhY5Y0F4leJIKg.', 'ROLE_CLIENTE');
+
+INSERT INTO VAGAS (id, codigo, status) VALUES (10, 'A-01', 'LIVRE');
+INSERT INTO VAGAS (id, codigo, status) VALUES (20, 'A-02', 'LIVRE');
+INSERT INTO VAGAS (id, codigo, status) VALUES (30, 'A-03', 'OCUPADA');
+INSERT INTO VAGAS (id, codigo, status) VALUES (40, 'A-04', 'LIVRE');

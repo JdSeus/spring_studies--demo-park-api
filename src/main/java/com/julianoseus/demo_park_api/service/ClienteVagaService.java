8a56a695ea16d06163556bafddde4ce5c4bf116c
JdSeus <julianoseusdev@outlook.com>
2025-08-25 11:46:45 -0300

    Make ClienteVagaService and EstacionamentoService

---
+++ src/main/java/com/julianoseus/demo_park_api/service/ClienteVagaService.java
@@ -0,0 +1,20 @@
+package com.julianoseus.demo_park_api.service;
+
+import com.julianoseus.demo_park_api.entity.ClienteVaga;
+import com.julianoseus.demo_park_api.repository.ClienteVagaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ClienteVagaService {
+
+    private final ClienteVagaRepository repository;
+
+    @Transactional
+    public ClienteVaga salvar(ClienteVaga clienteVaga) {
+        return repository.save(clienteVaga);
+    }
+
+}
